4f397192af17586f9d1d6af185926ee6885aac8c
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-28 12:13:06 -0300

    db h2

--- src/main/java/com/renan/crudspring/model/Course.java
+++ src/main/java/com/renan/crudspring/model/Course.java
@@ -6,19 +6,23 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 @Entity
+//@Table(name = "cursos")
 public class Course {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
     private String name;
 
-    @Column(length = 20 , nullable = false)
+    @Column(length = 10 , nullable = false)
     private String category;
 }
