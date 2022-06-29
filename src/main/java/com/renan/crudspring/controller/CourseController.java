6ac64878c2cb25fb216e2acbbd7d251a74d91d0a
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-29 14:37:03 -0300

    adiciona metodo post

--- src/main/java/com/renan/crudspring/controller/CourseController.java
+++ src/main/java/com/renan/crudspring/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.renan.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.renan.crudspring.model.Course;
@@ -23,5 +28,14 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED) 
+    public Course create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+        //return ResponseEntity.status(HttpStatus.CREATED)
+        //    .body(courseRepository.save(course));
     }
 }
