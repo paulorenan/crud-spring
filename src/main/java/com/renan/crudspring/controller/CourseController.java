b24cd23fa76191609550cca9cfe806d8408330ba
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-30 14:28:22 -0300

    metodo put

--- src/main/java/com/renan/crudspring/controller/CourseController.java
+++ src/main/java/com/renan/crudspring/controller/CourseController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,5 +40,13 @@ public class CourseController {
         return courseRepository.save(course);
         //return ResponseEntity.status(HttpStatus.CREATED)
         //    .body(courseRepository.save(course));
+    }
+
+    @PutMapping("/{id}")
+    public Course update(@RequestBody Course course, @PathVariable Long id) {
+        Course courseToUpdate = courseRepository.findById(id).get();
+        courseToUpdate.setName(course.getName());
+        courseToUpdate.setCategory(course.getCategory());
+        return courseRepository.save(courseToUpdate);
     }
 }
